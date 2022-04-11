08c942a109f160ec5154aa7a85c536e4e7323513
Emmanuel Fofeyin <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-04-11 16:16:21 +0000

    added code

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2022 Emmanuel-Fofeyin All rights reserved
+//
+// Created by: Emmanuel.Fofeyin
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function Converts Fehrenheit to Celsius.
+ */
+function calculate () {
+  // input
+  const fehrenheit = parseInt(document.getElementById('fehrenheit').value)
+
+
+  // process
+  const volume = (°F − 32) × 5/9 = °C
+
+
+  // output
+  document.getElementById('area').innerHTML = 'The Celsius temperature is: ' +  Celsius temperature.toFixed(2) + ' °C'
+}
